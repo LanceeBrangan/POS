e53192622d8dae1b5ddc9c61a681455e24e147f2
lance <lanceebrangan@gmail.com>
2026-09-13 14:18:24 +0800

    updated func for sale and saleitem

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -3,10 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Dabase\Eloquent\Attributes\Fillable;
+use Illuminate\Dabase\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable(['user_id', 'total', 'cash_tendered', 'change_amount', 'status', 'notes'])]
 class Sale extends Model
 {
-    //
+    protected $casts = [
+        'total' => 'decimal:2',
+        'cash_tendered' => 'decimal:2',
+        'change_amount' => 'decimal:2',
+    ];
+
+    public function items(): HasMany
+    {
+        return $this - hasMany(SaleItem::class);
+    }
 }
